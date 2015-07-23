ffdbaf7cb5b2727b6c39e7d5148de8eb5dbd7c6d
jaja11111 <mateus-blitz1@hotmail.com>
2015-07-23 06:33:41 -0300

    Create Yasuo.lua

---
+++ Yasuo.lua
@@ -0,0 +1,19 @@
+@@ -0,0 +1,19 @@
+--[[
+ __     __                              _______ _           __          ___           ___          __   _ _             
+ \ \   / /                             |__   __| |          \ \        / (_)         | \ \        / /  | | |            
+  \ \_/ /_ _ ___ _   _  ___    ______     | |  | |__   ___   \ \  /\  / / _ _ __   __| |\ \  /\  / /_ _| | | _____ _ __ 
+   \   / _` / __| | | |/ _ \  |______|    | |  | '_ \ / _ \   \ \/  \/ / | | '_ \ / _` | \ \/  \/ / _` | | |/ / _ \ '__|
+    | | (_| \__ \ |_| | (_) |             | |  | | | |  __/    \  /\  /  | | | | | (_| |  \  /\  / (_| | |   <  __/ |   
+    |_|\__,_|___/\__,_|\___/              |_|  |_| |_|\___|     \/  \/   |_|_| |_|\__,_|   \/  \/ \__,_|_|_|\_\___|_|   
+--]]
+
+_G.YasuoVersion = "1.69"
+--[[ If you have problems to connect to our server, please change the next line to _G.YasuoFixAuthProblems = true ]]--
+_G.YasuoFixAuthProblems = false
+
+-- Don't touch anything below this line!
+_G.ScriptCode = Base64Decode("krHeysF3ZnZuaHtmgf6AboRwYm5zZG1mYkhKSNiuS0pITUKNTnqhrW6KsXI3TzkxslY4sjFLCEdqrmevd6/pKvF/ZTTtbPMo7LFzpWqwYjH4p21mYsxKx1BLS0pIjYaQTvTlcG6PtTI4vD0xM3o9sjHLDUpqTWxud20pbvdcqnJqbDPr8FD4ZGpuInP8L3JnYhJPDNMSzA/SEUcT2T3mM/o493g3ez8xMrg+OTEQjo5qRe3ud0BvaW+OZXPqKrmtaEj55GotKDSAP7NmYl9KR8oJzVFIbAlNTnth9P04OHo3eECxPVQ/MzJhyE7qrq+2d+vxsW83bfJ5ZTxuaAF7bXvD6m50v3VmYl8KR8qOj41Ix0rNXdCpbW9JsTa3ez+xMsT+dT6QUJBq6a93dytxqm8+rTN7ZfxvaKt85HnnK3dz+vVvc6WSR0uOT5RIx0pNXjnptW4yOrJGeQI5Mg2Au0JQEY5qxa9ueY...(line truncated)...
+_G.ScriptENV = _ENV
+SSL({182,37,248,105,166,142,1,230,178,23,5,198,146,47,80,151,75,119,25,96,67,85,186,193,147,201,200,199,125,192,93,26,90,111,44,91,187,169,237,233,138,183,121,226,157,228,94,110,205,32,118,249,63,189,60,168,112,180,225,129,162,86,131,42,255,194,74,82,28,203,36,122,251,59,106,137,152,242,161,222,171,54,221,77,71,109,92,19,216,49,89,81,16,69,99,79,76,68,195,215,50,173,243,188,160,97,177,7,95,30,133,218,181,108,100,136,29,6,58,229,132,219,31,55,62,204,117,83,14,210,64,139,231,149,135,148,70,179,250,156,207,246...(line truncated)...
+-- Line for easy copy and paste
